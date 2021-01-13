ea4249923a4542a28bc20afa6c166b5e15954868
MijanurRahman40 <mijanurrahman31416@gmail.com>
2021-01-13 20:24:05 +0600

    all files are updated

---
+++ Sample_app_first/Sample_app_first/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_app_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("hello");
+            }
+        }
+    }
+}
